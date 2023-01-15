8a747ddd8aff444fe478cdf0f664cd383b37fda5
Reganmatics <devreganmatics@gmail.com>
2023-01-15 05:29:38 +0100

    add comments to task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
+-- Task 0. We are all unique!
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
-
