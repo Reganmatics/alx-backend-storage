c13131df3eabde9ce7436f79e683e4460b90eb4a
Reganmatics <devreganmatics@gmail.com>
2023-01-19 05:25:56 +0100

    add task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Task 0. Writing strings to Redis
+"""
+import redis
+import uuid
+from redis.typing import Union
+
+
+class Cache:
+    """
+    this class uses an instance of redis as a private variable
+    """
+    def __init__(self):
+        """
+        constructor method
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data:Union[str, bytes, int, float]) -> str:
+        """
+        args:
+            data -> a union of data types
+        return -> str
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
